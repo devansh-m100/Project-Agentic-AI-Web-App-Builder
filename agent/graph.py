a870573b4424807e374c6a4136d16507a86ea31c
Devansh M <devansh.m100@gmail.com>
2026-03-23 11:57:01 +0530

    refactor(states): extract Pydantic schemas into dedicated states module
    * Moved `File` and `Plan` Pydantic models from main file to states.py
    * Centralized schema definitions for LangGraph state management
    * Improved code organization and separation of concerns
    * Updated imports in main file to use schemas from states.py
    * Prepares project for scalable multi-agent / state-driven architecture
    
    Also verified and debugged the integration using PyCharm’s inbuilt debugger.

--- agent/graph.py
+++ agent/graph.py
@@ -1,32 +1,15 @@
 from dotenv import load_dotenv
-
 load_dotenv()
 
 from langchain_groq import ChatGroq
-
 llm = ChatGroq(model="openai/gpt-oss-120b")
 
-from pydantic import BaseModel, Field
+from prompts import *
+from states import *
 
 user_prompt = "create a simple calculator web application"
 
-prompt = f"""
-You are the PLANNER agent. Convert the user prompt into a COMPLETE engineering project plan
-
-User request: {user_prompt}
-"""
-
-class File(BaseModel):
-    path: str = Field(description="path to file to be created or modified")
-    purpose: str = Field(description="purpose of the file, e.g. 'main application logic', 'data processing module', etc.")
-
-class Plan(BaseModel):
-    name: str = Field(description="the name of the app to be built")
-    description: str = Field(description="the description of the app to be built")
-    techstack: str = Field(description="the techstack to be used for the app")
-    features: list[str] = Field(
-        description="the list of features to be used for the app, e.g. 'user authentication', 'data_visualization', etc.")
-    files: list[File] = Field(description="a list of files to be created, each with a 'path' and 'purpose'")
+prompt = planner_prompt(user_prompt)
 
 resp = llm.with_structured_output(Plan).invoke(prompt)
 
---
+++ agent/states.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, Field
+
+class File(BaseModel):
+    path: str = Field(description="path to file to be created or modified")
+    purpose: str = Field(description="purpose of the file, e.g. 'main application logic', 'data processing module', etc.")
+
+class Plan(BaseModel):
+    name: str = Field(description="the name of the app to be built")
+    description: str = Field(description="the description of the app to be built")
+    techstack: str = Field(description="the techstack to be used for the app")
+    features: list[str] = Field(
+        description="the list of features to be used for the app, e.g. 'user authentication', 'data_visualization', etc.")
+    files: list[File] = Field(description="a list of files to be created, each with a 'path' and 'purpose'")
